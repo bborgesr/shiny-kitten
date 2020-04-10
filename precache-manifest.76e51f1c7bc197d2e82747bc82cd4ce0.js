a759a02345126c9422be4071a7200814c1c842ea
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-04-11 00:55:13 +0100

    Updates

---
+++ precache-manifest.76e51f1c7bc197d2e82747bc82cd4ce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f2ca3e92d3c733b173329623af6833",
+    "url": "/shiny-kitten/index.html"
+  },
+  {
+    "revision": "6290143e1fc8f07b3634",
+    "url": "/shiny-kitten/static/css/main.c8161f08.chunk.css"
+  },
+  {
+    "revision": "8896a86b66e235b7d88a",
+    "url": "/shiny-kitten/static/js/2.913e6e42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shiny-kitten/static/js/2.913e6e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6290143e1fc8f07b3634",
+    "url": "/shiny-kitten/static/js/main.08f9b64a.chunk.js"
+  },
+  {
+    "revision": "09cf5e75e458d21f6384",
+    "url": "/shiny-kitten/static/js/runtime-main.6fe388ed.js"
+  },
+  {
+    "revision": "7dd6c46e6737238ba531ad273ea672a5",
+    "url": "/shiny-kitten/static/media/cat.7dd6c46e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://localhost:3000/precache-manifest.12869951a9a8cedfde67f201ad65766e.js"
+  "/shiny-kitten/precache-manifest.76e51f1c7bc197d2e82747bc82cd4ce0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:3000/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shiny-kitten/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
