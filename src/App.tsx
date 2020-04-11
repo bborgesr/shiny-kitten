b300b5e7f1b8753098c9de49a65709b0029c69bf
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-04-11 02:03:08 +0100

    create header and about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Homepage from './components/Homepage';
+import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import Homepage from './components/Homepage';
+import About from './components/About';
 
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route
           exact
           path='/'
           render={(props) => <Homepage {...props} text={'Shiny Kitten'} />}
         />
+        <Route path='/about' component={About} />
       </Switch>
       <Footer />
     </>
---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './About.css';
+
+function About() {
+  return <div className='about'>An app about shiny kittens</div>;
+}
+
+export default About;
